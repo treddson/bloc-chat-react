12ff6ac76b841c0a1445de3f8862b90f29ba470a
Taylor Short <treddshort@gmail.com>
2018-06-02 20:56:22 -0700

    add firebase database

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import RoomList from './components/RoomList';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA_7VZru5wYLmL3k5lC-FVzD2MmVaaP-B8",
+  authDomain: "bloc-chat-react-3a40c.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-3a40c.firebaseio.com",
+  projectId: "bloc-chat-react-3a40c",
+  storageBucket: "bloc-chat-react-3a40c.appspot.com",
+  messagingSenderId: "518580362062"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Available Rooms</h1>
+        <RoomList 
+            firebase= { firebase } />
       </div>
     );
   }
