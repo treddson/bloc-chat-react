6eb66fcf8dacdeab94967ba39f3303086b862538
Taylor Short <treddshort@gmail.com>
2018-06-03 12:50:43 -0700

    fixed issue with firebase database

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -14,9 +14,8 @@ class RoomList extends Component {
     componentDidMount() {
         this.roomsRef.on('child_added', snapshot => {
             const room = snapshot.val();
-            // room.key = snapshot.key;
-            console.log(room);
-            this.setState({ rooms: this.state.rooms.concat( (room) ) })
+            room.key = snapshot.key;
+            this.setState({ rooms: this.state.rooms.concat( room ) })
         });
     }
 
