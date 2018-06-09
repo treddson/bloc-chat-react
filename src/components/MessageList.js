aa6bbb0e8143a74248f16d649863c8e90da58236
Taylor Short <treddshort@gmail.com>
2018-06-09 06:57:22 -0700

    create message-list component and add firebase reference

---
+++ src/components/MessageList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+
+class MessageList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+        this.roomsRef = this.props.firebase.database().ref('rooms');
+    }
+
+    componentDidMount() {
+        this.roomsRef.on('child_added', snapshot => {
+            const room = snapshot.val();
+            room.key = snapshot.key;
+            this.setState({ rooms: this.state.rooms.concat( room ) })
+        });
+    }
+
+
+  render() {
+    return (
+        <div>
+       
+        </div>
+    );
+  }
+}
+
+export default MessageList;
