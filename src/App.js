cffbb18e2505211134a870f59708c937462b2b03
Taylor Short <treddshort@gmail.com>
2018-06-15 18:35:17 -0700

    updating master branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import RoomList from './components/RoomList';
+import MessageList from './components/MessageList';
 import * as firebase from 'firebase';
 
 // Initialize Firebase
@@ -16,14 +17,35 @@ firebase.initializeApp(config);
 
 
 class App extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        activeRoom: "",
+        activeMessage: ""
+      }
+      this.setActiveRoom = this.setActiveRoom.bind(this);
+      this.setMessage = this.setMessage.bind(this);
+    }
+
+    setActiveRoom(roomId) {
+      console.log(roomId)
+      this.setState({ 
+        activeRoom: roomId
+
+      })
+    }
+    
+    setMessage(message) {
+      this.setState({ activeMessage: message })
+    }
+
   render() {
     return (
       <div className="App">
         <h1>Bloc Chat</h1>
-        <RoomList 
-            firebase= { firebase } 
-            createRoom={() => this.createRoom()}/>
-            
+        <RoomList firebase= { firebase } createRoom={() => this.createRoom() } setActiveRoom={this.setActiveRoom } />  
+        <MessageList firebase = { firebase } /> 
+        
       </div>
     );
   }
