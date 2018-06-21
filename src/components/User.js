8fe9879f5c15c1537dbf846e5726f9262bda1da7
Taylor Short <treddshort@gmail.com>
2018-06-21 07:21:51 -0700

    create User component and add boilerplate

---
+++ src/components/User.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+
+class User extends Component {
+    constructor(props) {
+        super(props);
+        this.state = [
+
+        ]
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+render() {
+    return (
+        <div>
+
+        </div>
+    )
+  }
+
+
+}
+
+
+
+export default User;
