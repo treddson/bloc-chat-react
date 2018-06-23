342acc3b2daf9c840c289814194155c804bcc321
Taylor Short <treddshort@gmail.com>
2018-06-22 21:23:40 -0700

    create setUser function

--- src/components/User.js
+++ src/components/User.js
@@ -12,34 +12,26 @@ class User extends Component {
         this.signOut = this.signOut.bind(this);
     }
 
-
     componentDidMount() {
-        this.props.firebase.auth().onAuthStateChanged( user => { this.props.setUser(user)
-            this.setState({user})
+        this.props.firebase.auth().onAuthStateChanged( user => { 
+            this.props.setUser(user)
         });
     }
-
 
     signIn() {
         const provider = new this.props.firebase.auth.GoogleAuthProvider();
         this.props.firebase.auth().signInWithPopup(provider);
-        console.log('signing in');
     }
 
     signOut() {
         this.props.firebase.auth().signOut();
     }
 
-
-
-
-
 render() {
     return (
         <div>
             <button className='sign-in' onClick={this.signIn}>Sign in</button>
-            <button className='sign-out' onClick={this.signOut}>Sign out</button>
-            
+            <button className='sign-out' onClick={this.signOut}>Sign out</button>  
         </div>
     );
   }
