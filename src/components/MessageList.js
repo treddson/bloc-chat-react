a9eeaf2b8aecfaab64e81105a4f0a713fa73aa16
Taylor Short <treddshort@gmail.com>
2018-07-13 19:06:37 -0700

    further testing...

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -92,10 +92,9 @@ class MessageList extends Component {
 
     createMessage(e) {
         e.preventDefault();
-        console.log(this.props.activeRoom.key + ' ...this is the active room');
         this.messagesRef.push({
             content: this.state.newMessage,
-            roomId: this.state.activeRoom.key,
+            roomId: this.props.activeRoom,
             username: !this.props.username ? 'Guest' : this.props.username.displayName,
             sentAt: this.props.firebase.database.ServerValue.TIMESTAMP
         });
@@ -120,7 +119,7 @@ class MessageList extends Component {
             <div className='message-list'> 
                 <ul>
                     { this.state.messages.map( (message, index) => {
-                        if (this.props.activeRoom.key === message.roomId) {
+                        if (this.props.activeRoom === message.roomId) {
                             return <li key={ index }> {message.username} says: {message.content} </li>
                         }
                     // console.log(this.props.activeRoom)
@@ -128,8 +127,8 @@ class MessageList extends Component {
                     })}
                 </ul>
                 <form className="new-message" onSubmit={ (e) => this.createMessage(e) }>
-                    <input type="text" value={this.state.newMessage} onChange={ (e) => this.handleChange(e)} />
-                    <input type="submit" value="Send" />
+                    <input placeholder="your message" type="text" value={this.state.newMessage} onChange={ (e) => this.handleChange(e)} />
+                    <input className="btn btn-light btn-sm" type="submit" value="Send" />
                 </form>
             </div>
         )
